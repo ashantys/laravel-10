b6f2a5cb1f456256ab8510eedb6f85a51e5dd764
ashantys <ashantys.m@gmail.com>
2023-10-22 20:04:25 -0600

    feat(middleware):middleware practica

--- bootcamp/routes/web.php
+++ bootcamp/routes/web.php
@@ -3,29 +3,30 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('welcome');
+
+
+/*
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+auth = verifica si el usuario ha hecho login con anterioridad
+ y tiene una sesion valida, si no es asi, lo llevara a la vista login
+ el middleware verified = si el usuario a verificado su correo
+*/
+
 Route::middleware('auth')->group(function () {
+
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/chirps', function () {
+        return 'Welcome to our chirps page';
+    })->name('chirps.index');
 });
 
 require __DIR__.'/auth.php';
